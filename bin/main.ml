5d7234630f90a60302c0ff6e2ed3bd04c8c17ad2
Liam Berry <liamandberry@gmail.com>
2024-08-21 18:06:40 +0200

    pointer issues

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,5 @@
 let new_peer = Ctypes.make Wglib.Wireguard.Wg_peer.wg_peer
 
-(* wg_peer new_peer = { *)
-(* 	.flags = WGPEER_HAS_PUBLIC_KEY | WGPEER_REPLACE_ALLOWEDIPS *)
-(* }; *)
 let peer_key =
   Wglib.Wgapi.Key.generate_private_key () |> Wglib.Wgapi.Key.generate_public_key
 
@@ -11,16 +8,13 @@ let () =
     (Ctypes.CArray.start peer_key)
 
 let () =
-  Ctypes.setf new_peer Wglib.Wireguard.Wg_peer.flags
-    (Unsigned.UInt32.of_int
-       (Wglib.Wireguard.Wg_peer.Wg_peer_flags.wgpeer_has_public_key
-      lor Wglib.Wireguard.Wg_peer.Wg_peer_flags.wgpeer_replace_allowedips))
+  Ctypes.setf new_peer Wglib.Wireguard.Wg_peer.flags (Unsigned.UInt32.of_int 6)
 
 let private_key = Wglib.Wgapi.Key.generate_private_key ()
 let () = print_endline (Wglib.Wgapi.Key.to_base64 private_key)
 let public_key = Wglib.Wgapi.Key.generate_public_key private_key
 let () = print_endline (Wglib.Wgapi.Key.to_base64 public_key)
-let name = "wgtest9"
+let name = "wgtest0"
 let new_device = Ctypes.make Wglib.Wireguard.Wg_device.wg_device
 let () = Ctypes.setf new_device Wglib.Wireguard.Wg_device.name name
 
@@ -30,22 +24,27 @@ let () =
 
 let () =
   Ctypes.setf new_device Wglib.Wireguard.Wg_device.wg_device_flags
-    (Int64.of_int
-       (Wglib.Wireguard.Wg_device.Wg_device_flags.wgdevice_has_private_key
-      lor Wglib.Wireguard.Wg_device.Wg_device_flags.wgdevice_has_listen_port))
+    (Int64.of_int 10)
 
 let () =
   Ctypes.setf new_device Wglib.Wireguard.Wg_device.private_key
     (Ctypes.CArray.start private_key)
 
 let () =
-  Ctypes.setf new_device Wglib.Wireguard.Wg_device.first_peer
-    (Ctypes.addr new_peer)
+  print_endline
+    ("Name: "
+    ^ Ctypes.getf
+        (new_device |> Ctypes.addr |> Ctypes.( !@ ))
+        Wglib.Wireguard.Wg_device.name)
+(* let () = *)
+(*   Ctypes.setf new_device Wglib.Wireguard.Wg_device.first_peer *)
+(*     (Ctypes.addr new_peer) *)
+(* let () = *)
+(*   Ctypes.setf new_device Wglib.Wireguard.Wg_device.last_peer *)
+(*     (Ctypes.addr new_peer) *)
 
-let () =
-  Ctypes.setf new_device Wglib.Wireguard.Wg_device.last_peer
-    (Ctypes.addr new_peer)
-
+let err = Wglib.Wireguard.wg_del_device name
+let () = Printf.printf "wg_del_device: %d\n" err
 let err = Wglib.Wireguard.wg_add_device name
 let () = Printf.printf "wg_add_device: %d\n" err
 let err = Wglib.Wireguard.wg_set_device (Ctypes.addr new_device)
